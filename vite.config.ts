e650cd46e3b75b5a4b26c9b4f0937c2cf9f9c96f
Qainguin <slimaster14@gmail.com>
2025-09-24 14:19:53 -0500

    Sonda and Remove vex dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import Sonda from "sonda/sveltekit";
 
 export default defineConfig({
   build: {
     sourcemap: true,
     minify: true,
   },
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [tailwindcss(), sveltekit(), Sonda({ server: false, deep: true })],
 });
