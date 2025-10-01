a31db68bcdcebe7108031415075578a013526a1f
Qainguin <slimaster14@gmail.com>
2025-10-01 09:30:27 -0500

    Add Guides

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -3,12 +3,14 @@
 	import 'prism-code-editor/prism/languages/cpp';
 	import '$lib/themes/birds.css';
 	import 'prism-code-editor/layout.css';
+	import 'prism-code-editor/guides.css';
 	import { onMount, untrack } from 'svelte';
 	import { createEditor, type PrismEditor } from 'prism-code-editor';
 	import { defaultCommands } from 'prism-code-editor/commands';
 	import { cursorPosition } from 'prism-code-editor/cursor';
 	import { fs } from '$lib/loader.svelte';
 	import type { BuildOutput, Issue } from '$lib/builder';
+	import { indentGuides } from 'prism-code-editor/guides';
 
 	interface Props {
 		code: string;
@@ -45,7 +47,7 @@
 			await fs.writeFile(path, e, { mode: 0o777, encoding: 'utf8' });
 		});
 
-		editor.addExtensions(defaultCommands(), cursorPosition());
+		editor.addExtensions(defaultCommands(), cursorPosition(), indentGuides());
 	});
 
 	function cleanErrors() {
@@ -67,13 +69,12 @@
 					const err: Issue = errors[e];
 
 					if (err.filepath.replace(`/${id}`, '') !== path)
-
-					editor!.lines[err.lineNumber].classList.add('error-line');
+						editor!.lines[err.lineNumber].classList.add('error-line');
 					currentErrorLines.push(editor!.lines[err.lineNumber]);
 				}
 			}
 		});
-	})
+	});
 </script>
 
 <div id="editor" class="grid h-[calc(100vh-64px)] w-full overflow-y-auto"></div>
